4317a22cb0cb0602445b4b5f3321e83f21fd6276
DiCollars <dicollarsfamily@gmail.com>
2020-11-06 13:27:21 +0300

    Music web application

---
+++ Cassette/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Cassette.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cassette.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserContext db;
+
+        public AccountController(UserContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(u => u.Login == model.Login && u.Password == model.Password);
+                if (user != null)
+                {
+                    await Authenticate(model.Login);
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "ERROR");
+            }
+            return View(model);
+        }
+
+
+        private async Task Authenticate(string userName)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ Cassette/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Cassette.Models;
+using System.IO;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Cassette.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UserContext _db;
+        public HomeController(UserContext db)
+        {
+            _db = db;
+        }
+
+        [Authorize]
+        public IActionResult Index(string Id)
+        {
+            //Change the way
+            DirectoryInfo path = new DirectoryInfo(@"A:\Stack\Projects\petProjects\Mvc\Cassette\Cassette\wwwroot\music");
+            FileInfo[] Files = path.GetFiles("*.mp3");
+            List<string> FullNamesOfSongs = new List<string>();
+
+            foreach (var song in Files)
+            {
+                FullNamesOfSongs.Add(song.Name);
+            }
+
+            if (Id == default)
+            {
+                Id = FullNamesOfSongs.FirstOrDefault();
+            }
+
+            ViewBag.SongName = Id;
+
+            return View(FullNamesOfSongs);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
